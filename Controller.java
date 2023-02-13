7cb426924dee2b016c18e2b4816185d2a1ed2fd4
ArtakQA <120332154+ArtakQA@users.noreply.github.com>
2023-02-13 19:11:55 +0400

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,22 +1,15 @@
 public class Controller {
     public static void main(String[] args) {
-        Testing testing = new Testing();
-        testing.testToInt();
-        testing.negativeTestInt();
-        testing.testDoubleInt();
-        testing.testToIntZero();
-        testing.testToDoubleMinus();
-        testing.testDouble();
-        testing.testReset();
+
         SpreadSheet sp = new SpreadSheet(5, 4);
         sp.setNewSheet(0,0, "test5");
-        sp.setNewSheet(0,1,"1");
-        sp.setNewSheet(0,2,"2");
-        sp.setNewSheet(0,3,"3");
+        sp.setNewSheet(0,1,1);
+        sp.setNewSheet(0,2,1.2);
+        sp.setNewSheet(0,3,3);
         sp.setNewSheet(1,0,"test4");
-        sp.setNewSheet(1,1,"1");
-        sp.setNewSheet(1,2,"2");
-        sp.setNewSheet(1,3,"3");
+        sp.setNewSheet(1,1,"2022-02-03");
+        sp.setNewSheet(1,2,"2022-02-03");
+        sp.setNewSheet(1,3,1);
         sp.setNewSheet(2,0,"test3");
         sp.setNewSheet(2,1,"1");
         sp.setNewSheet(2,2,"2");
